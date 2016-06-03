f3a5ca0e3dad7a731e5eecf61a75f299100940df
Abhishek Gupta <abhishek.gupta@successivesoftwares.com>
2016-06-03 18:30:55 +0530

    Navigator switching

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('task', function () {
+Route::get('task',['as'=>'mytask'], function () {
     return view('tasks');
 });
 
--- resources/views/layouts/navigator.blade.php
+++ resources/views/layouts/navigator.blade.php
@@ -1,10 +1,11 @@
 @if (!Auth::guest())
+    <?php $routeName= Route::current()->getName(); ?>
 <div class="container">
 <ul class="nav nav-tabs">
-    <li role="presentation" class="active"><a onclick="active()" href="{{url('/')}}">Welcome</a></li>
-  <li role="presentation" ><a onclick="active()" href="{{url('/task')}}">Members</a></li>
-  <li role="presentation"><a onclick="active()" href="{{url('/task/create')}}">Add Member</a></li>
-  <li role="presentation"><a onclick="active()" href="#">Messages</a></li>
+    <li role="presentation" class="<?=($routeName==''? 'active': '')?>"><a  href="{{url('/')}}">Welcome</a></li>
+  <li role="presentation" class="<?=($routeName=='task.index'? 'active': '')?>" ><a href="{{url('/task')}}">Members</a></li>
+  <li role="presentation" class="<?=($routeName=='task.create'? 'active': '')?>" ><a  href="{{url('/task/create')}}">Add Member</a></li>
+  <li role="presentation" ><a  href="#">Messages</a></li>
 </ul>
 </div>
 @endif
@@ -12,13 +13,12 @@
 @yield('content')
 
 <script type="text/javascript">
-    function active(){
-        
-    }
+
 $(document).ready(function() {
-    $(".nav a").on("click", function(){
+    $(".nav li a").on("click", function(){
         $(".nav").find(".active").removeClass("active");
         $(this).parent().addClass("active");
     });
 });
+
 </script>
